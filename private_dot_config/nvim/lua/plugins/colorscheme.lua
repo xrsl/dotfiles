8cdd0a9a134f8303cb6f17a435111b45b8ef25f7
Resul <ahmetresulal@gmail.com>
2026-09-12 17:22:17 +0200

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/colorscheme.lua

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,26 +1,44 @@
+local config = function()
+  require("rose-pine").setup({
+    variant = "moon",
+    disable_italics = true,
+    groups = {
+      punctuation = "highlight_lo",
+    },
+  })
+  vim.cmd("colorscheme rose-pine")
+end
+
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    opts = {
-      flavour = "mocha", -- latte, frappe, macchiato, mocha
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        neotree = true,
-        treesitter = true,
-        notify = true,
-        mini = true,
-        which_key = true,
-      },
-    },
-  },
-
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = config,
   },
 }
+--return {
+--  {
+--    "catppuccin/nvim",
+--    name = "catppuccin",
+--    priority = 1000,
+--    opts = {
+--      flavour = "mocha", -- latte, frappe, macchiato, mocha
+--      integrations = {
+--        cmp = true,
+--        gitsigns = true,
+--        neotree = true,
+--        treesitter = true,
+--        notify = true,
+--        mini = true,
+--        which_key = true,
+--      },
+--    },
+--  },
+--
+--  {
+--    "LazyVim/LazyVim",
+--    opts = {
+--      colorscheme = "catppuccin",
+--    },
+--  },
+--}
